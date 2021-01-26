fddaa92323949987313fccc3ca8be21f735a5f82
AlexP <alex.p.panteleev@gmail.com>
2021-01-26 17:52:20 +0300

    [f] public assets

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
-    <script src="{{asset('public/js/app.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}"></script>
 </head>
 <body>
 <div id="app"></div>
